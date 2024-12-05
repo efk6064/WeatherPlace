6de4a7fb722799fe05884c5ef0ff0b33f7a74466
efk6064 <efk6064@gmail.com>
2024-12-05 04:52:42 +0200

    update api

--- client/src/features/weather/WeatherApi.js
+++ client/src/features/weather/WeatherApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl="http://localhost:4000/api/weather";
+const baseUrl="http://localhost:3000/api/weather";
 
 export const getWeatherByCity=(city)=>{
     return axios.get(`${baseUrl}/${city}`);
